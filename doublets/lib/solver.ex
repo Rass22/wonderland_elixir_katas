5588c6e8319c7214477993198a447ba755e6a9af
Rass22 <100615564+Rass22@users.noreply.github.com>
2022-03-18 00:41:25 -0600

    Update solver.ex

--- doublets/lib/solver.ex
+++ doublets/lib/solver.ex
@@ -1,13 +1,41 @@
 defmodule Doublets.Solver do
-
-  def words do
+  import String
+  defp words do
     "./resources/words.txt"
       |> File.stream!
       |> Enum.to_list
   end
-
   def doublets(word1, word2) do
-    "make me work"
+    cond do
+      String.length(word1) == String.length(word2) ->
+        leng = [word1 | doublets(word1, word2, words())]
+        cond do
+          Enum.member?(leng, "sin respuesta") -> []
+          true -> leng
+        end
+      true -> []
+    end
   end
 
+  def doublets(word1, word2, dic) when word1 != word2 do
+    hola = Enum.map(dic, &trim(&1))
+      |> Enum.filter(fn x -> String.length(word1) == String.length(x) end) |> List.delete(word1)
+    x = hd Enum.sort_by(hola, &{Simetric.Levenshtein.compare(&1, word1), Simetric.Levenshtein.compare(&1, word2)}, :asc) |> IO.inspect()
+    dif_letra =  Enum.zip_reduce(String.to_charlist(word1), String.to_charlist(x), 0, fn x, y, acc ->
+      cond do
+      x != y -> acc + 1
+      true -> acc
+      end
+    end)
+    IO.inspect(word1)
+    IO.inspect(x)
+    IO.inspect(dif_letra)
+    cond do
+      dif_letra == 1 -> [x | doublets(x, word2, hola)]
+      true -> ["sin respuesta"]
+    end
+  end
+  def doublets(word1, word2, _) when word1 == word2 do
+     []
+  end
 end
